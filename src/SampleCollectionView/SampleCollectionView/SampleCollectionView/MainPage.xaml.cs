df7a97cab35e3e2f4a800baea0df358eb5e33e05
Autyan <autyan@outlook.com>
2021-05-27 18:02:20 +0800

    Add Sample Project

---
+++ src/SampleCollectionView/SampleCollectionView/SampleCollectionView/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Xamarin.Forms;
+
+namespace SampleCollectionView
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            CollectionView.ItemsSource = new[]
+                                         {
+                                             "avator.jpg",
+                                             "avator.jpg",
+                                             "avator.jpg",
+                                             "avator.jpg"
+                                         };
+        }
+    }
+}
